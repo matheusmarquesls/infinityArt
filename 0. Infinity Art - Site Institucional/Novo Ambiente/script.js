31dabb1e35bb57085f04f8552d66822ed041d900
Fernando-Henrique-SpTech <fernando.soares@sptech.school>
2024-05-24 10:53:21 -0300

    Atualizando elementos da tabela

--- 0. Infinity Art - Site Institucional/Novo Ambiente/script.js
+++ 0. Infinity Art - Site Institucional/Novo Ambiente/script.js
@@ -1,13 +1,18 @@
-// script.js
-
 $(document).ready(function () {
     $('#minhaTabela').DataTable({
+        dom: '<"custom-header"lf>rt<"custom-footer"ip>',
         language: {
             info: 'Páginas _PAGE_ de _PAGES_',
             infoEmpty: 'Nenhum registro disponível',
-            infoFiltered: '(filtered from _MAX_ total records)',
+            infoFiltered: '(filtrado de _MAX_ registros totais)',
             lengthMenu: '_MENU_ Registros por páginas',
-            zeroRecords: 'Nada encontrado'
+            zeroRecords: 'Nada encontrado',
+            search: "",  // Desativa o label do input de busca
+            searchPlaceholder: "Digite para buscar...",  // Placeholder do input de busca
+            paginate: {
+                previous: "Anterior",
+                next: "Próximo"
+            }
         },
     });
 });
